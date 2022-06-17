c95603416bdf1ac4bf8e2116b6710c7009abe803
joao marcelo <joao_marcelo.22@hotmail.com>
2022-06-17 02:12:57 -0300

    docs:

--- authentication.js
+++ authentication.js
@@ -1,7 +1,22 @@
 var authEmailPassButton = document.getElementById('authEmailPassButton');
 var authAnonymouslyButton = document.getElementById('authAnonymouslyButton');
+var crateUserButton = document.getElementById('createUserButton')
 
 var emailInput = document.getElementById('emailInput');
 var passwordInput = document.getElementById('passwordInput');
 
 var displayName = document.getElementById('displayName')
+
+creatUserButton.addEventListener('click', function(){
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(emailInput.Value, passwordInput.Value)
+        .then(function (){
+            alert('Bem vindo' + emailInput.value);
+        })
+        .catch(function(error){
+            console.error(error.code);
+            console.error(error.menssage);
+            alert('Falha ao cadastrar, verifique erro no console.')
+        });
+})
